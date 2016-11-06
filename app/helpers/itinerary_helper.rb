239db978d41bb08a248e98c488ba13d8b9e0af1e
Derek Edwards <dedwards8@gmail.com>
2016-11-06 18:31:49 -0500

    Bring in itinerary email header

--- app/helpers/itinerary_helper.rb
+++ app/helpers/itinerary_helper.rb
@@ -16,4 +16,57 @@ module ItineraryHelper
     return "#{place.address}, #{place.city}, #{place.state}"
   end
 
+  def humanized_start_time
+    return format_time self.start_time
+  end
+
+  def humanized_end_time
+    return format_time self.end_time
+  end
+
+  def format_time(time)
+    time.strftime('%l:%M %p').strip
+  end
+
+  def humanized_duration options={}
+    return humanized_time_in_seconds self.duration, options
+  end
+
+  def humanized_walk_time options={}
+    return humanized_time_in_seconds self.walk_time, options
+  end
+
+  def humanized_time_in_seconds(time_in_seconds, options = {})
+
+    time_in_seconds = time_in_seconds.to_i
+    hours = time_in_seconds/3600
+    minutes = (time_in_seconds - (hours * 3600))/60
+
+    time_string = ''
+
+    if time_in_seconds > 60*60*24 and options[:days_only]
+      count = hours / 24
+      return count.to_s + " " + "Day"
+    end
+
+    if hours > 0
+      time_string << hours.to_s + " " + "hour "
+    end
+
+    if minutes > 0 || (hours > 0 and !options[:suppress_minutes])
+      time_string << minutes.to_s + " " + "min"
+      time_string << "s" if minutes != 1
+    end
+
+    if time_in_seconds < 60
+      time_string = TranslationEngine.translate_text(:less_than_one_minute)
+    end
+
+    if options[:days_only]
+      time_string = (hours/24.round).to_s + " day"
+    end
+
+    time_string
+  end
+
 end
