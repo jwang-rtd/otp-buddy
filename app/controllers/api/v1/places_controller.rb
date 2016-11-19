be0b545f870b036896e934292b42ef1b06439f88
Derek Edwards <dedwards8@gmail.com>
2016-11-18 23:11:00 -0500

    Fixes #29, If the first 3 numbers of a stop match, send back results.

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -6,20 +6,22 @@ module Api
         #Get the Search String
         search_string = params[:search_string]
         include_user_pois = params[:include_user_pois]
-        max_results = (params[:max_results] || 10).to_i
+        max_results = (params[:max_results] || 5).to_i
 
         locations = []
-        count = 0
 
         #Check for exact match on stop code
         #Cut out white space and remove wildcards
-        stripped_string = search_string.tr('%', '').strip
-        stop = Landmark.stops.where(stop_code: stripped_string).first
-        if stop
-          locations.append(stop.build_place_details_hash)
+        stripped_string = search_string.tr('%', '').strip.to_s + '%'
+        if stripped_string.length >= 4 #Only check once 3 numbers have been entered
+          stops = Landmark.stops.where('stop_code LIKE ?', stripped_string).limit(max_results)
+          stops.each do |stop|
+            locations.append(stop.build_place_details_hash)
+          end
         end
 
         # Global POIs
+        count = 0
         landmarks = Landmark.get_by_query_str(search_string, max_results, true)
         landmarks.each do |landmark|
           locations.append(landmark.build_place_details_hash)
