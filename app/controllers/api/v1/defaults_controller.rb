d6fb247e68dbd7e6263fcf329c1e80262dc729ef
Derek Edwards <dedwards8@gmail.com>
2018-08-13 11:42:29 -0400

    Defaults controller should use key instead of code when referencing OtpBuddy Settings.

--- app/controllers/api/v1/defaults_controller.rb
+++ app/controllers/api/v1/defaults_controller.rb
@@ -3,11 +3,11 @@ module Api
     class DefaultsController < Api::V1::ApiController
 
       def index
-        render json: Setting.send(self.code)
+        render json: Setting.send(self.key)
       end
 
       def create
-        oc = Setting.where(code: self.code).first_or_initialize
+        oc = Setting.where(key: self.code).first_or_initialize
         oc.value = params["data"].to_json
         oc.save
         render json: {status: 200, message: "Success"}
