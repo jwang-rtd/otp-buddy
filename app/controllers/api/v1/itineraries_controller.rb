5bfbd89f52796f006e44afb7f7645613eabdb68e
Derek Edwards <dedwards8@gmail.com>
2018-12-14 11:36:23 -0500

    Ensure that the itinerary emails lists them in the same order that they were requested.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -113,7 +113,9 @@ module Api
           email_addresses = email_itinerary[:email_addresses]
           subject = email_itinerary[:subjectLine]
           ids = email_itinerary[:itineraries].collect { |x| x[:id] }
-          itineraries = Itinerary.where(id: ids)
+           
+          #get the itineraries and ensures that they are retuned in the same order they were requested
+          itineraries = Itinerary.find(ids).index_by(&:id).values_at(*ids)
 
           # for subject, get first trip
           trip = itineraries.first.request.trip
